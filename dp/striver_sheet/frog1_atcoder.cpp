9966d1aa21cb281edaae3baf61ff409c990ee3ca
celestial_worthy <ayushsingh09lucknow@gmail.com>
2026-08-27 02:07:01 +0530

    Modify frog1_atcoder.cpp for height-based logic
    Refactor frog problem solution to use heights instead of weights and values.

--- dp/striver_sheet/frog1_atcoder.cpp
+++ dp/striver_sheet/frog1_atcoder.cpp
@@ -6,25 +6,20 @@ int main(){
     ios::sync_with_stdio(false); 
     cin.tie(nullptr); 
 
-    ll N,total_weight;
-    cin>>N>>total_weight;
-    vector<ll>values(N),weights(N);
-    for(ll i=0;i<N;i++)cin>>weights[i]>>values[i];
-    vector<vector<ll>>dp(N,vector<ll>(total_weight+1,0));
+    ll N;
+    cin>>N;
+    vector<ll>heights(N);
+    for(ll i=0;i<N;i++)cin>>heights[i];
+    vector<ll>dp(N+1,0);
 
     //base case
-    for(ll w=weights[0];w<=total_weight;w++){
-        dp[0][w]=values[0];
-    }
-
+    dp[0]=0;
+    dp[1]=abs(heights[1]-heights[0]);
     //main logic
-    for(ll i=1;i<N;i++){
-        for(ll w=1;w<=total_weight;w++){
-            ll not_take=dp[i-1][w];
-            ll take=INT_MIN;
-            if(weights[i]<=w)take=values[i]+dp[i-1][w-weights[i]];
-            dp[i][w]=max(take,not_take);
-        }
+    for(ll i=2;i<N;i++){
+        ll jump1=dp[i-1]+abs(heights[i]-heights[i-1]);
+        ll jump2=dp[i-2]+abs(heights[i]-heights[i-2]);
+        dp[i]=min(jump1,jump2);
     }
-    cout<<dp[N-1][total_weight];
+    cout<<dp[N-1];
 }
